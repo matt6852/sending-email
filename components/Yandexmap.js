c4be465bb532599c674a538661ee3ae0f86a200f
matt6852 <prorokwow@mail.ru>
2021-05-07 09:42:23 +0300

    added yandexmap

---
+++ components/Yandexmap.js
@@ -0,0 +1,28 @@
+import { YMaps, Map, Placemark } from "react-yandex-maps";
+// import styles from '../../styles/Home.module.css'
+
+const mapData = {
+  center: [55.751574, 37.573856],
+  zoom: 5,
+};
+
+const coordinates = [
+  [55.684758, 37.738521],
+  [52.7317, 41.4433],
+];
+
+const Yandexmap = () => (
+  <YMaps>
+    <Map defaultState={mapData}
+    width ={"100"}
+    height = {"400px"}
+    // className = {styles.center}
+    >
+      {coordinates.map((coordinate,index) => (
+        <Placemark key ={index} geometry={coordinate} />
+      ))}
+    </Map>
+  </YMaps>
+);
+
+export default Yandexmap
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 import { useState } from "react";
+import Yandexmap from "../components/Yandexmap";
 
 export default function Home() {
   const [name, setName] = useState("");
@@ -9,48 +10,56 @@ export default function Home() {
   const [phone, setPhone] = useState("");
   const [submitted, setSubmitted] = useState(false);
 
-  const handleSubmit = (e) => { 
-  e.preventDefault()
-  console.log('Sending')
-let data = {
-    name,
-    email,
-    phone
-  }
-fetch('/api/contact', {
-    method: 'POST',
-    headers: {
-      'Accept': 'application/json, text/plain, */*',
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(data)
-  }).then((res) => {
-    console.log('Response received')
-    if (res.status === 200) {
-      console.log('Response succeeded!')
-      setSubmitted(true)
-      setName('')
-      setEmail('')
-      setPhone('')
-      setSubmitted(true)
-
-    }
-  })
-}
-
-
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Sending");
+    let data = {
+      name,
+      email,
+      phone,
+    };
+    fetch("/api/contact", {
+      method: "POST",
+      headers: {
+        Accept: "application/json, text/plain, */*",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    }).then((res) => {
+      console.log("Response received");
+      if (res.status === 200) {
+        console.log("Response succeeded!");
+        setSubmitted(true);
+        setName("");
+        setEmail("");
+        setPhone("");
+        setSubmitted(true);
+      }
+    });
+  };
 
   return (
     <div className={styles.container}>
+      <div>
+        <h2>Яндекс карта</h2>
+      </div>
+
+      <div className={styles.center}>
+        <Yandexmap />
+      </div>
       {submitted ? (
         <div>
           <h1>
             Спасибо за то что выбрали нас мы свяжимся с вами в ближаещее время
-            <button onClick ={()=> setSubmitted(false)}>закрыть</button>
+            <button onClick={() => setSubmitted(false)}>закрыть</button>
           </h1>
         </div>
       ) : (
         <form className={styles.main}>
+          <div> <h2>
+            Регистрация
+          </h2>
+          </div>
           <label htmlFor="name">Name</label>
           <input
             type="text"
