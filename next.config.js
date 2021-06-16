4631a34ab2403a3d84d4b1f5164adc03d1c70329
matt6852 <prorokwow@mail.ru>
2021-06-16 15:17:52 +0300

    added envs

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  env: {
+    PASSWORD: process.env.PASSWORD,
+    EMAIL: process.env.EMAIL,
+    HOST: process.env.HOST,
+    PORT: process.env.PORT,
+  
+    // development: {
+    //   compact: true,
+    // },
+  },
+};
+// PASSWORD = 25800852asdf
+// EMAIL =sss.1993@internet.ru
+// HOST = 	smtp.mail.ru
+// PORT = 465
